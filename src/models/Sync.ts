a3e2c8cee5dbdd1881200debf221d30c151db4d8
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2023-04-19 23:55:55 +0300

    sync composition using generics

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,14 +1,18 @@
 import axios, { AxiosPromise } from 'axios';
-import { UserProps } from './User';
 
-export class Sync {
+// ? We want to have constrain with id property
+interface HasId {
+  id?: number;
+}
+
+export class Sync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
     return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): AxiosPromise {
+  save(data: T): AxiosPromise {
     const { id } = data;
 
     if (id) {
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,17 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
 export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
 }
-// http://localhost:3000/users
+
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
 
