db2dd14d13933a9cb9e9cd3b6b1fdc3acdbeb9e6
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2023-04-22 14:47:38 +0300

    refactor User and create static prcofiged user constructor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1, name: 'John', age: 25 });
+const user = User.buildUser({ id: 1, name: 'John', age: 25 });
 
 // ? The parentheses (....) invoking the on function in Eventing class
-user.on('save', () => {
+user.on('change', () => {
   console.log(user);
 });
 
-user.save();
+user.fetch();
