fdd006582978692ed0c1f7e905fa39b771cbaf7d
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2023-04-22 15:20:23 +0300

    make collection generic

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-import { User, UserProps } from './User';
 
-export class Collection {
-  models: User[] = [];
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
-  constructor(public rootUrl: string) {}
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   get on() {
     return this.events.on;
@@ -17,9 +16,8 @@ export class Collection {
 
   fetch(): void {
     axios.get(this.rootUrl).then((response: AxiosResponse) => {
-      response.data.forEach((value: UserProps) => {
-        const user = User.buildUser(value);
-        this.models.push(user);
+      response.data.forEach((value: K) => {
+        this.models.push(this.deserialize(value));
       });
 
       this.trigger('change');
