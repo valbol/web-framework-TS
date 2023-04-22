b3f046ec7e6ca6389454a4d760f2b51cb989a5dd
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2023-04-22 15:36:47 +0300

    static func collection in User class

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,8 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
+import { Collection } from './Collection';
+
 export interface UserProps {
   name?: string;
   age?: number;
@@ -17,6 +19,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
