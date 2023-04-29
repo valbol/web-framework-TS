c3921620af079d45787f285b249e7ce9f024c0a9
Valery_Boltavsky <valery.boltavsky@checkmarx.com>
2023-04-29 23:00:13 +0300

    nested rendering

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { User, UserProps } from './models/User';
+import { UserEdit } from './views/UserEdit';
 import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'John', age: 30 });
@@ -6,8 +7,9 @@ const user = User.buildUser({ name: 'John', age: 30 });
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log(userEdit);
 } else {
   throw new Error('Root element not found');
 }
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,11 +1,21 @@
 import { Model, HasId } from '../models/Model';
 
 export abstract class View<T extends Model<K>, K extends HasId> {
+  regions: { [key: string]: Element } = {};
+
   constructor(public parent: Element, public model: T) {
     this.bindModel();
   }
-  abstract eventsMap(): { [key: string]: () => void };
+
   abstract template(): string;
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
 
   bindModel(): void {
     this.model.on('change', () => {
@@ -15,6 +25,7 @@ export abstract class View<T extends Model<K>, K extends HasId> {
 
   bindEvents(fragment: DocumentFragment): void {
     const eventsMap = this.eventsMap();
+
     for (let eventKey in eventsMap) {
       const [eventName, selector] = eventKey.split(':');
 
@@ -24,14 +35,31 @@ export abstract class View<T extends Model<K>, K extends HasId> {
     }
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    const regionsMap = this.regionsMap();
+
+    for (let key in regionsMap) {
+      const selector = regionsMap[key];
+      const element = fragment.querySelector(selector);
+
+      if (element) {
+        this.regions[key] = element;
+      }
+    }
+  }
+
+  onRender(): void {}
+
   render(): void {
     this.parent.innerHTML = '';
 
-    // ? template gives the ability to take a string and transfer it to html
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
 
     this.parent.append(templateElement.content);
   }
